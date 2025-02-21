1e3630095d6ceb77093ae62f375f9aa45bc4ea92
pradnyabirdawade <166538166+pradnyabirdawade@users.noreply.github.com>
2025-02-21 18:56:24 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,15 @@
+#Q1. Write a Python program to find the number of times 4 appears in the tuple. 
+
+# Define the tuple
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Count occurrences of 4
+count_4 = tuplex.count(4)
+
+# Print the result
+print("Number of times 4 appears:", count_4)
+
+
+***Output
+Number of times 4 appears: 3
+***
---
+++ q2.py
@@ -0,0 +1,16 @@
+#Q2.Write a Python program to convert a list to a tuple. Input: listx = [5, 10, 7, 4, 15, 3]
+
+
+
+# Define the list
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple
+tuplex = tuple(listx)
+
+# Print the result
+print("Converted tuple:", tuplex)
+
+***Output
+Converted tuple: (5, 10, 7, 4, 15, 3)
+***
